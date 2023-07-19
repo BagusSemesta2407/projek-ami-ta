be429e580aa8f72e436f1ec57c4cb92450457046
BagusSemesta2407 <semestabagus24@gmail.com>
2023-07-20 02:17:36 +0700

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,22 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('content')
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>AMI | POLITEKNIK NEGERI SUBANG</title>
+    <link rel="stylesheet" href="{{ asset('assets-lp/css/maicons.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('assets-lp/css/bootstrap.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('assets-lp/vendor/animate/animate.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('assets-lp/css/theme.css') }}">
+
+</head>
+
+<body>
     <section class="vh-100" style="background-color: #ffffff;">
         <div class="container py-5 h-100">
             <div class="row d-flex justify-content-center align-items-center h-100">
@@ -8,8 +24,8 @@
                     <div class="card" style="border-radius: 1rem;">
                         <div class="row g-0">
                             <div class="col-md-6 col-lg-6 d-none d-md-block">
-                                <img src="{{ asset('bg-login.jpg') }}"
-                                    alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem;" />
+                                <img src="{{ asset('bg-login.jpg') }}" alt="login form" class="img-fluid"
+                                    style="border-radius: 1rem 0 0 1rem;" />
                             </div>
                             <div class="col-md-6 col-lg-6 d-flex align-items-center">
                                 <div class="card-body p-4 p-lg-5 text-black">
@@ -42,8 +58,9 @@
                                         <div class="form-outline mb-4">
                                             <label class="form-label" for="">Password</label>
                                             <input id="password" type="password"
-                                                class="form-control form-control-lg @error('password') is-invalid @enderror" name="password"
-                                                 autocomplete="current-password" placeholder="Masukkan Password">
+                                                class="form-control form-control-lg @error('password') is-invalid @enderror"
+                                                name="password" autocomplete="current-password"
+                                                placeholder="Masukkan Password">
 
                                             @error('password')
                                                 <span class="invalid-feedback" role="alert">
@@ -66,4 +83,16 @@
             </div>
         </div>
     </section>
-@endsection
+    <script src="{{ asset('assets-lp/js/jquery-3.5.1.min.js') }}"></script>
+
+    <script src="{{ asset('assets-lp/js/bootstrap.bundle.min.js') }}"></script>
+
+    <script src="{{ asset('assets-lp/js/google-maps.js') }}"></script>
+
+    <script src="{{ asset('assets-lp/vendor/wow/wow.min.js') }}"></script>
+
+    <script src="{{ asset('assets-lp/js/theme.js') }}"></script>
+
+</body>
+
+</html>
